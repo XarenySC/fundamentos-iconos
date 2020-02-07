d2a62a61f38d8493bbc60de3867960b531da6802
Xareny <SALCO@MacBook-Pro-de-Xareny.local>
2020-02-07 17:52:36 -0600

    tarea de horoscopo

--- switch/script.js
+++ switch/script.js
@@ -1,12 +1,58 @@
-var tuSigno = promptn('escribe tu signo zodiacal')
+var tuSigno = prompt('Hola escribe tu signo zodiacal')
 
-switch (tuSigno) {
-  case 'aries':
-    alert('lorem')
+
+switch (tuSigno.toUpperCase()) {
+  case 'ARIES':
+    alert('Aries verá en su horizonte profesional muchas oportunidades de conseguir un trabajo en el extranjero y expandir sus expectativas hacia el éxito que busca en su carrera, será un momento de arduo trabajo para Aries si quiere conseguir esas ambiciones laborales. Los planes para el fin de semana podrían superar las expectativas si los nacidos bajo este signo del Zodiaco se abren a nuevas experiencias, sin preocuparse tanto por el dinero. La posición de los astros fortalecerá el vínculo sentimental de Aries con su pareja, es momento de dar el paso a un mayor compromiso; si está soltero, Aries deberá plantarle cara a ciertas situaciones para que surja el amor con esa persona que está conociendo. Los altos niveles de activación le van a causar mucha ansiedad que afectará la salud de Aries.')
     break;
 
-  case 'tauro':
-    alert('loremjxjscxndsjcndjcnds')
+  case 'TAURO':
+    alert('Hoy se presentarán grandes oportunidades en el trabajo para que Tauro dé un giro inesperado a su carrera, aunque habrá decisiones importantes que tomar, Tauro deberá renovarse para que vengan los cambios que necesita para volver a brillar en este 2020, a nivel profesional y económico. Algunas discusiones con alguien cercano mantendrán a Tauro distanciado, de su círculo de amigos. Las habilidades de seducción de los nacidos bajo este signo del Zodiaco aumentarán estos días, con la posición de los astros a su favor, ideal si está soltero y conquistando un corazón que le cautivó; si tiene pareja, despertará celos si Tauro no muestra el amor en su relación, podría estar jugando con fuego. Será un momento ideal para que Tauro se plantee reorganizar su alimentación y añadir frutas y verduras a su dieta y que le aporte vitaminas y minerales para fortalecer su salud.  ')
+    break;
+
+
+  case 'GÉMINIS':
+    alert('Con la influencia de los astros, Géminis podrá darse cuenta y valorar todo lo que ha conseguido hasta ahora a nivel profesional, gracias a su esfuerzo y dedicación en el trabajo Géminis ha creado muchas oportunidades para evolucionar en el terreno laboral y económico durante este 2020. A nivel social, los nacidos bajo este signo del Zodiaco recibirán grandes consejos de las mujeres de su entorno, esto le permitirá tener nuevas perspectivas hacia su futuro personal a corto plazo. En el plano sentimental, Géminis recibirá hoy algunas sorpresas de su pareja que reafirmaran su relación; pero si está soltero, podría haber ciertas complicaciones con esa persona que ha cautivado a Géminis para que el amor de ambos se fragüe. El cansancio acumulado empezará a preocupar a Géminis porque comenzará a afectar su salud.')
+    break;
+
+  case 'CÁNCER':
+    alert('Hoy Cáncer recuperará su confianza en el trabajo que se ha visto afectada por acontecimientos pasados, con ello, Cáncer podrá afrontar nuevos retos profesionales y progresar en la evolución de su carrera. En el aspecto personal los nacidos bajo este signo del Zodiaco estará pasando su mejor momento, conocerá a nuevas amistades que se aportarán mutuamente grandes perspectivas en sus vidas. En el amor, tanto si tiene o no pareja, se verá enriquecida por nuevas experiencias que los astros presagian para Cáncer. Para seguir con la buena racha en su salud, será conveniente que Cáncer evite los excesos alimenticios y deje el sedentarismo a un lado. ')
+    break;
+
+  case 'LEO':
+    alert('A pesar de estar cerca del éxito por el cual Leo ha estado trabajando duro, deberá confiar en sí mismo y mostrarse firme en sus decisiones para que Leo alcance sus objetivos económicos y de trabajo. Los astros auguran que es momento para que los nacidos bajo este signo del Zodiaco organicen un encuentro familiar y social, pero deberá tener cuidado de no crear discordia entre los invitados. Leo tendrá ciertas discrepancias en su relación, esto generará un ambiente tenso con la pareja o posible conquista si Leo está soltero, esto podría acabar interponiéndose en su amor. Hoy será un buen día para que Leo se relaje y conecte con sus emociones y necesidades, esto hará que su salud física y mental se equilibre.')
+    break;
+
+  case 'VIRGO':
+    alert('A pesar de estar cerca del fin de semana, Virgo está empezando a saturarse por la carga de trabajo y esto le hará sentir agobiado, y esto no permitirá que sus ideas fluyan. Hoy el cuerpo pedirá un respiro, Virgo podría quedar a última hora con los amigos, esto le reconfortará el alma. Cuando Virgo se dé cuenta de los sentimientos hacia su pareja y valore a la persona que tiene al lado, empezará a disfrutar su relación, si los nacidos bajo este signo del Zodiaco están solteros, la influencia de los astros incitará para que el amor se acerque. Cuando Virgo aprenda desconectarse de las preocupaciones y equilibre cuerpo y mente, gozará de buena salud.')
+    break;
+
+  case 'LIBRA':
+    alert('Por más que Libra quiera evitar un problema en el trabajo no lo quedará más remedio que hacerle frente e intentar solventarlo antes de que se haga más grande y acabe en un error irreparable. Para disfrutar de la vida será conveniente que los nacidos bajo este signo del Zodiaco aprendan a vivir en el presente y compartir momentos de calidad con su círculo de amigos más cercano. La influencia de los astros hará gala en la vida sentimental de Libra permitiendo que exprese sus sentimientos que le harán sentirse más involucrados en sus relaciones, en especial en el plano del amor, tenga o no una pareja. Si Libra sigue con este estado de estrés ansiedad, su salud acabará resintiéndose de una manera u otra')
+    break;
+
+  case 'ESCORPIO':
+    alert('Escorpio se habrá embarcado en un proyecto que se ha vuelto un reto personal que si se deja llevar por el orgullo sacará adelante a pesar de los problemas que le está causando, solo para demostrar que es el mejor y satisfacer su ego profesional. Con el fin de semana muy próximo, los nacidos bajo este signo del Zodiaco aprovecharán para pasar más tiempo en el plano social. En el terreno sentimental, no será tan fácil los astros presagian que Escorpio podría estar provocando un alejamiento con su pareja o conquista por no dedicar tiempo y compromiso a su relación. Hoy Escorpio empezará a sentir el cansancio acumulado ya que su salud no estará con la energía característica')
+    break;
+
+  case 'SAGITARIO':
+    alert('A pesar de haber tenido unos días complicados en el trabajo hoy Sagitario dará con la solución a un problema que ha surgido, esto además de hacer que termine el día más tranquilo le dará cierta satisfacción a Sagitario sabiendo que está en el buen camino. Su círculo de amigos traerá buenas noticias, será conveniente que Sagitario no deje de frecuentarlos aun esté cargado de trabajo. Los astros auguran que no será el mejor momento para que los nacidos bajo este signo del Zodiaco empiecen las comparaciones de su pareja con las anteriores, esto le llevará al fracaso en su relación, si está soltero, será el momento en el cual si Sagitario se da la oportunidad podría dejar entrar al amor a su vida. Si Sagitario tiene consideración con su cuerpo evitará consecuencias en su salud.')
+    break;
+
+  case 'CAPRICORNIO':
+    alert('Aunque atraviese cierta inestabilidad en el trabajo, será importante que Capricornio se centre en sus responsabilidades laborales, se avecinan decisiones significativas para el progreso en el devenir profesional de Capricornio. Volcarse en el trabajo hará que se distancie de su círculo de amigos y perjudique su vida social. Hoy podría recibir una invitación a salir de alguien que está conociendo, los nacidos bajo este signo del Zodiaco deberán dejarse llevar por esas ilusiones, los astros presagian que podría surgir el amor; si tiene pareja, habrá sorpresas que fortalezcan la relación. El exceso de café estará empezando a crear tensión en su organismo afectando así la salud de Capricornio.')
+    break;
+
+  case 'ACUARIO':
+    alert('La creatividad de Acuario se verá hoy potenciada tras recibir una noticia que despertará la motivación laboral de este signo, esto será el primer paso para que Acuario brille en el trabajo con grandes expectativas de éxito para el 2020. Será un momento de reflexión, que permitirá a Acuario tomar algunas decisiones que le lleven a un cambio radicar a nivel personal y emocional. Con la conjunción de los astros, saldrá a relucir el lado seductor de los nacidos bajo este signo del Zodiaco, habrá conquistas, aunque el verdadero amor podría estar en alguien del pasado que dejó ir; si tiene pareja, será un momento para fortalecer la relación. El ejercicio será bueno para las articulaciones y esas molestias musculares que perjudican la salud de Acuario.')
+    break;
+
+  case 'PISCIS':
+    alert('Si Piscis pretende dar un giro a su vida profesional, es momento de plantearse nuevos objetivos para propiciar ese cambio que necesita para despertar la motivación que se está disipando en su carrera, esto permitirá que Piscis se encamina hacia un trabajo de éxito. El amor para los nacidos bajo este signo del Zodiaco será favorable debido a la conjunción de los , por ello, aquellos soltero podrían conocer a alguien que le devolverá la ilusión en sus sentimientos, si tiene una relación, Piscis deberá poner hoy atención a su pareja quien le reclama cuidado y cariño, antes de que la relación sentimental acabe distanciándose. Si Piscis deja a un lado los excesos físicos y emocionales verá un gran cambio en su salud.')
+    break;
+
+  default:
+    alert('Intenta de nuevo este signo no existe en nuestra constelación')
     break;
 
   //TERMINAR TODOS 
